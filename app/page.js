ff9d1beb509bc4382c17bde632426874e821d9ac
behrmart <bfelipemm@gmail.com>
2024-07-02 18:07:12 -0600

    Test changes in Chakra UI

--- app/page.js
+++ app/page.js
@@ -1,8 +1,16 @@
-
 import Fortune from "./components/fortune/page";
-import { Button, Link, Flex, Square, Center, Text, Box, Spacer } from "@chakra-ui/react";
+import {
+  Button,
+  Link,
+  Flex,
+  Square,
+  Center,
+  Text,
+  Box,
+  Spacer,
+  VStack,
+} from "@chakra-ui/react";
 import Navbar from "./components/navbar/navbar";
-
 
 //import { AuthProvider } from "./context/authcontext";
 
@@ -10,29 +18,30 @@ export default function Home() {
   return (
     //<AuthProvider>
     <main>
-      <Navbar></Navbar>
-      <Flex>
-        <Link  href="/worldtimeapi"> 
-        <Center bg="green.500">
-          <Box p="6" >WT API</Box>
-        </Center>
-        </Link>
-        <Link  href="/videojs"> 
-        <Center bg="blue.500">
-          <Box p="6">Video</Box>
-        </Center>
-        </Link>
+      <VStack align="stretch">
+        <Navbar />
+        <Box>
+          <Link href="/worldtimeapi">
+            <Center bg="green.500">
+              <Box p="6">WT API</Box>
+            </Center>
+          </Link>
+          <Link href="/videojs">
+            <Center bg="blue.500">
+              <Box p="6">Video</Box>
+            </Center>
+          </Link>
+          <Link href="/worldtimeapi">
+            <Center bg="tomato">
+              <Box p="6">Home</Box>
+            </Center>
+          </Link>
+        </Box>
         <Spacer />
-        <Link  href="/worldtimeapi"> 
-        <Center bg="tomato">
-          <Box p="6">Home</Box>
-        </Center>
-        </Link>
-        
-      </Flex>
-
-     
-      <Fortune></Fortune>
+        <Box>
+          <Fortune />
+        </Box>
+      </VStack>
     </main>
     //</AuthProvider>
   );
