8886325bff7aaa1c438654074afb5aa4fbdf750f
behrmart <bfelipemm@gmail.com>
2025-04-03 09:15:15 -0600

    Used DeepSeek to make responsive the main page.

--- app/page.js
+++ app/page.js
@@ -1,41 +1,39 @@
 import Footer from "./components/footer/page";
-// import Worldtimeapi from "./components/worldtimeapi/page";
 import Pokeapi from "./components/pokemonapi/page";
 import Catapi from "./components/catapi/page";
-import {
-  Button,
-  Link,
-  Flex,
-  Square,
-  Center,
-  Text,
-  Box,
-  Spacer,
-  HStack,
-  Heading,
-} from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 import Navbar from "./components/navbar/navbar";
 import Openweatherapi from "./components/openweatherapi/page";
 
-//import { AuthProvider } from "./context/authcontext";
-
 export default function Home() {
   return (
-    //<AuthProvider>
     <main>
       <Navbar />
 
-      <Flex>
-        <Pokeapi />
-        {/* <Worldtimeapi /> */}
-        <Catapi />
-        <Openweatherapi />
+      <Flex
+        direction={["column", "row", "row"]} // column on mobile, row otherwise
+        wrap="wrap"
+        justify="center"
+        align="stretch"
+        gap={4}
+        p={4}
+      >
+        <Box width={["100%", "50%", "25%"]} minWidth="300px">
+          <Pokeapi />
+        </Box>
+
+        <Box width={["100%", "50%", "25%"]} minWidth="300px">
+          <Catapi />
+        </Box>
+
+        <Box width={["100%", "50%", "25%"]} minWidth="300px">
+          <Openweatherapi />
+        </Box>
       </Flex>
 
-      <Box>
+      <Box mt={8}>
         <Footer />
       </Box>
     </main>
-    //</AuthProvider>
   );
 }
