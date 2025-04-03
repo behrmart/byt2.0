4b5a5365b1aed87c579b134129c78a34ca2dfe0a
behrmart <bfelipemm@gmail.com>
2025-04-03 10:38:28 -0600

    Updated Documentation

--- app/page.js
+++ app/page.js
@@ -1,3 +1,29 @@
+/* 
+BEHRMART BYT2.0
+Just a simple exercise to test external public/private APIs. 
+
+by Bernardo Felipe Martinez Meave
+https://github.com/behrmart
+
+Application is built on Next.js/React (Using the App Router). Uses one propietary API 
+running on my own server to get the Linux Fortunes. This FortuneServer API is running 
+using Docker and and Mongo DB (Cloud Mongo DB Atlas). 
+The Docker container runs a Node.js Server to query a Mongo Atlas DB using Mongoose.
+
+Docker container for MongoDB running on Ubuntu Linux.
+sudo docker run -d --name mongolin -p27017:27017 -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret mongo
+sudo docker start mongolin
+
+FortuneVaquitaServer
+https://github.com/behrmart/FortuneVaquitaServer
+
+Docker container Links to MongoDB Atlas
+
+Also uses public RESTful APIs: 
+https://pokeapi.co 
+https://api.thecatapi.com 
+https://api.openweathermap.org */
+
 import Footer from "./components/footer/page";
 import Pokeapi from "./components/pokemonapi/page";
 import Catapi from "./components/catapi/page";
